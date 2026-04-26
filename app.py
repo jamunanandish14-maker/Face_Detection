906704e5972c1a974900a8eee434775961601552
jamunanandish14-maker <jamunanandish14@gmail.com>
2026-04-26 13:42:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,14 @@
 import streamlit as st
+import pickle
 import numpy as np
-import tensorflow as tf
-from PIL import Image
-import os
-import urllib.request
 
-MODEL_URL = "PASTE_MODEL_LINK_HERE"
-MODEL_PATH = "face_mask_model.h5"
+model = pickle.load(open("model.pkl","rb"))
 
-if not os.path.exists(MODEL_PATH):
-    urllib.request.urlretrieve(MODEL_URL, MODEL_PATH)
+st.title("Prediction App")
 
-model = tf.keras.models.load_model(MODEL_PATH)
+v1 = st.number_input("Feature 1")
+v2 = st.number_input("Feature 2")
 
-st.title("Face Mask Detection")
-
-file = st.file_uploader("Upload Image", type=["jpg","png","jpeg"])
-
-if file is not None:
-    image = Image.open(file).resize((128,128))
-    st.image(image)
-
-    img = np.array(image)/255.0
-    img = img.reshape(1,128,128,3)
-
-    pred = model.predict(img)
-
-    if pred[0][0] > 0.5:
-        st.error("Without Mask")
-    else:
-        st.success("With Mask")
+if st.button("Predict"):
+    pred = model.predict([[v1,v2]])
+    st.write(pred)
